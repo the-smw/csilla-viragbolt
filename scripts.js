af4570e636a5be64977e7e2959a86fbd71b463fa
the-smw <62156804+the-smw@users.noreply.github.com>
2020-11-07 13:30:50 +0100

    added base html and layout

---
+++ scripts.js
@@ -0,0 +1,160 @@
+//Toggle function for SMALL SCREEN
+
+
+$(document).ready(function () {
+  $(".toggle-1").click(function () {
+    $(".first").toggle(1000);
+    $(".second").hide(200);
+    $(".third").hide(200);
+    $(".fourth").hide(200);
+    $(".fifth").hide(200);
+    $(".sixth").hide(200);
+  });
+});
+
+
+
+$(document).ready(function () {
+  $(".toggle-2").click(function () {
+    $(".second").toggle(1000);
+    $(".first").hide(200);
+    $(".third").hide(200);
+    $(".fourth").hide(200);
+    $(".fifth").hide(200);
+    $(".sixth").hide(200);
+  });
+});
+
+
+
+$(document).ready(function () {
+  $(".toggle-3").click(function () {
+    $(".third").toggle(1000);
+    $(".first").hide(200);
+    $(".second").hide(200)
+    $(".fourth").hide(200);
+    $(".fifth").hide(200);
+    $(".sixth").hide(200);
+  });
+});
+
+
+
+$(document).ready(function () {
+  $(".toggle-4").click(function () {
+    $(".fourth").toggle(1000);
+    $(".first").hide(200);
+    $(".second").hide(200)
+    $(".third").hide(200);
+    $(".fifth").hide(200);
+    $(".sixth").hide(200);
+  });
+});
+
+
+
+$(document).ready(function () {
+  $(".toggle-5").click(function () {
+    $(".fifth").toggle(1000);
+    $(".first").hide(200);
+    $(".second").hide(200)
+    $(".third").hide(200);
+    $(".fourth").hide(200);
+    $(".sixth").hide(200);
+  });
+});
+
+
+
+$(document).ready(function () {
+  $(".toggle-6").click(function () {
+    $(".sixth").toggle(1000);
+    $(".first").hide(200);
+    $(".second").hide(200)
+    $(".third").hide(200);
+    $(".fourth").hide(200);
+    $(".fifth").hide(200);
+  });
+});
+
+
+
+
+
+// Toggle function for PC
+
+$(document).ready(function () {
+  $(".toggle-one").click(function () {
+    $(".one").toggle(1000);
+    $(".two").hide(200);
+    $(".three").hide(200)
+    $(".four").hide(200);
+    $(".five").hide(200);
+    $(".six").hide(200);
+  });
+});
+
+
+
+$(document).ready(function () {
+  $(".toggle-two").click(function () {
+    $(".two").toggle(1000);
+    $(".one").hide(200);
+    $(".three").hide(200)
+    $(".four").hide(200);
+    $(".five").hide(200);
+    $(".six").hide(200);
+  });
+});
+
+
+
+$(document).ready(function () {
+  $(".toggle-three").click(function () {
+    $(".three").toggle(1000);
+    $(".one").hide(200);
+    $(".two").hide(200)
+    $(".four").hide(200);
+    $(".five").hide(200);
+    $(".six").hide(200);
+  });
+});
+
+
+
+$(document).ready(function () {
+  $(".toggle-four").click(function () {
+    $(".four").toggle(1000);
+    $(".one").hide(200);
+    $(".two").hide(200)
+    $(".three").hide(200);
+    $(".five").hide(200);
+    $(".six").hide(200);
+  });
+});
+
+
+
+$(document).ready(function () {
+  $(".toggle-five").click(function () {
+    $(".five").toggle(1000);
+    $(".one").hide(200);
+    $(".two").hide(200)
+    $(".three").hide(200);
+    $(".four").hide(200);
+    $(".six").hide(200);
+  });
+});
+
+
+
+$(document).ready(function () {
+  $(".toggle-six").click(function () {
+    $(".six").toggle(1000);
+    $(".one").hide(200);
+    $(".two").hide(200)
+    $(".three").hide(200);
+    $(".four").hide(200);
+    $(".five").hide(200);
+  });
+});
